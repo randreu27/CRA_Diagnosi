c6dffd227bcfe89b449516d064804d2c2233bfea
randreu27 <ra270404@gmail.com>
2022-12-21 11:05:05 +0100

    update

--- 607(2).pl
+++ 607(2).pl
@@ -10,7 +10,7 @@ inici:-
     writeln('Vols tornar a fer el diagnostic: ? (si/no)'),
     read(Resp), nl,
     (Resp == si ->
-    writeln('Nova sessi√≥.'),
+    writeln('Nova sessiÛ.'),
      fail % backtrack al repeat
     ;   writeln('Acabem, doncs.'),
     ! % Acabem el backtrack
@@ -22,7 +22,7 @@ hypotesis(mal_de_muntanya_agut_greu):- mal_de_muntanya_agut_greu.
 hypotesis(mal_de_muntanya_agut_suau):- mal_de_muntanya_agut_suau.
 hypotesis(no_mal_de_muntanya).
 
-/* Verificaci√≥ */
+/* VerificaciÛ */
 
 verifica(S):-
  (si(S)
@@ -33,12 +33,12 @@ verifica(S):-
  fail ;
  pregunta(S))).
 
-/* Regles d'identificaci√≥ */
+/* Regles d'identificaciÛ */
 
 /* Mal de Muntanya Agut Suau */
 
 mal_de_muntanya_agut_suau:- gran_altura,
-    n√†usees_i_vomits.
+    n‡usees_i_vomits.
 
 mal_de_muntanya_agut_suau:-
     gran_altura,
@@ -47,23 +47,23 @@ mal_de_muntanya_agut_suau:-
 
 mal_de_muntanya_agut_suau:-
     gran_altura,
-    verifica(dificultat_per_el_esfor√ß_fisic),
-    verifica(inflamaci√≥_de_mans_peus_i_cara),
+    verifica(dificultat_per_el_esforÁ_fisic),
+    verifica(inflamaciÛ_de_mans_peus_i_cara),
     verifica(dolors_musculars).
 
 mal_de_muntanya_agut_suau:-
     gran_altura,
-    verifica(batec_cardiac_r√†pid),
+    verifica(batec_cardiac_r‡pid),
     verifica(dificultat_per_respirar).
 
 mal_de_muntanya_agut_suau:-
     gran_altura,
     verifica(insomni),
     verifica(irritabilitat),
-    verifica(p√®rdua_de_gana).
+    verifica(pËrdua_de_gana).
 
-gran_altura:- verifica(est√†s_a_gran_altura).
-n√†usees_i_vomits:- verifica(n√†usees_i_vomits).
+gran_altura:- verifica(est‡s_a_gran_altura).
+n‡usees_i_vomits:- verifica(n‡usees_i_vomits).
 
 /* Mal de Muntanya Agut Greu */
 
@@ -73,11 +73,11 @@ mal_de_muntanya_agut_greu:-
 
 mal_de_muntanya_agut_greu:-
     mal_de_muntanya_agut_suau,
-    verifica(congesti√≥).
+    verifica(congestiÛ).
 
 mal_de_muntanya_agut_greu:-
     mal_de_muntanya_agut_suau,
-    verifica(pell_p√†lida).
+    verifica(pell_p‡lida).
 
 mal_de_muntanya_agut_greu:-
     mal_de_muntanya_agut_suau,
@@ -85,12 +85,12 @@ mal_de_muntanya_agut_greu:-
 
 mal_de_muntanya_agut_greu:-
     mal_de_muntanya_agut_suau,
-    verifica(autoreclusi√≥_social).
+    verifica(autoreclusiÛ_social).
 
 /* Part per preguntar a l'usuari */
 
 pregunta(Pregunta):-
-    write('Tens el s√≠mptoma de: '),
+    write('Tens el sÌmptoma de: '),
     write(Pregunta),
     write('? '),
     read(Resposta),
